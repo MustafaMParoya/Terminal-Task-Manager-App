255dd476925c0e94a4df94432e98955a8f354b8f
Mustafa Paroya <143643428+MustafaMParoya@users.noreply.github.com>
2024-03-11 17:00:12 -0400

    changes
    task managemt

--- src/task_manager/TaskManagerApp.java
+++ src/task_manager/TaskManagerApp.java
@@ -3,7 +3,7 @@ package task_manager;
 import task_manager.services.TaskManagerSystem;
 
 public class TaskManagerApp {
-  public static void main(String[] args) {
+  public static <String> void main(String[] args) {
     TaskManagerSystem.run();
   }
 }
